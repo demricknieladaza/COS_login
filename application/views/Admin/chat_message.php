186cfc98655cecccb51fbc4abe9242fda0d49a64
Demrick Adaza <demricknieladaza@gmail.com>
2018-06-05 01:07:59 +0800

    chat progress
    polish pm's

--- application/views/Admin/chat_message.php
+++ application/views/Admin/chat_message.php
@@ -23,12 +23,46 @@ function submitChat() {
 	xmlhttp.send();
 
 }
+function submitChats() {
+	if(form1.uname.value == '' || form1.msg.value == '') {
+		alert("ALL FIELDS ARE MANDATORY!!!");
+		return;
+	}
+	var form = document.getElementById("form1");
+	var uname = form1.uname.value;
+	var msg = form1.msg.value;
+	var userid = form1.userid.value;
+	var xmlhttp = new XMLHttpRequest();
+	
+	xmlhttp.onreadystatechange = function() {
+		if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			document.getElementById('chatlogs').innerHTML = xmlhttp.responseText;
+			form.reset();
+		}
+	}
+	
+	xmlhttp.open('GET','http://localhost/php-chat/insert.php?key=mysend&user_id='+userid+'&uname='+uname+'&msg='+msg,true);
+	xmlhttp.send();
+
+}
 
 $(document).ready(function(e){
+	var fname = $(this).attr("id");
+	var x = document.getElementById("chatlogs");
+	var x1 = document.getElementById("chatlogsa");
+	var y = document.getElementById("btn-chata");
+	var y1 = document.getElementById("btn-chatmy");
+	document.getElementById("userid").value = fname;
 	$.ajaxSetup({
 		cache: false
 	});
-	setInterval( function(){ $('#chatlogs').load("http://localhost/php-chat/logs.php?key=getall&uname=<?php echo $this->session->userdata('userdata')['fname']; ?>"); }, 2000 );
+	if(x1.style.display == "block" && y1.style.display == "block" ){
+		setInterval( function(){ $('#chatlogsa').load("http://localhost/php-chat/logs.php?key=getmy&fname="+fname+"&user_id=<?php echo $this->session->userdata('userdata')['id']; ?>"); }, 2000 );
+	}
+	else{
+		setInterval( function(){ $('#chatlogs').load("http://localhost/php-chat/logs.php?key=getall&uname=<?php echo $this->session->userdata('userdata')['fname']; ?>"); }, 2000 );
+	}
+	
 });
 
 </script>
@@ -47,13 +81,13 @@ $(document).ready(function(e){
 </div>
 <form id="form1" name="form1"> 
 	<input type="hidden" name="uname" value="<?php echo $this->session->userdata('userdata')['fname']; ?>" /> <br />
-	<input type="text" name="userid" id="userid" value="" /> <br />
+	<input type="hidden" name="userid" id="userid" value="" /> <br />
 <div class="panel-footer">
     <div class="input-group">
         <input id="btn-input" name="msg" type="text" class="form-control input-sm" placeholder="Type your message here..." />
         <span class="input-group-btn">
-            <a type="submit" class="btn btn-warning btn-sm" id="btn-chat all" onclick="submitChat()" >Send</a>
-            <a type="submit" class="btn btn-warning btn-sm" id="btn-chat my" style="display:none;" onclick="submitChats()" >Send</a>
+            <a type="submit" class="btn btn-warning btn-sm" id="btn-chata" onclick="submitChat()" >Send</a>
+            <a type="submit" class="btn btn-warning btn-sm" id="btn-chatmy" style="display:none;" onclick="submitChats()" >Sends</a>
         </span>
     </div>
 </div>
--- application/views/Admin/pm_chat.php
+++ application/views/Admin/pm_chat.php
@@ -3,7 +3,7 @@
 		<?php foreach ($users as $user): ?>
 			<?php if($user->id == $this->session->userdata('userdata')['id']): ?>
 			<?php else: ?>
-				<li class="userchat" id="<?php echo $user->id; ?>" ><?php echo $user->fname; ?></li>
+				<li class="userchat" id="<?php echo $user->fname; ?>" ><?php echo $user->fname; ?></li>
 			<?php endif; ?>
 		<?php endforeach; ?>
 	</div>
@@ -11,43 +11,23 @@
 
 <script>
 
-function submitChats() {
-	if(form1.uname.value == '' || form1.msg.value == '') {
-		alert("ALL FIELDS ARE MANDATORY!!!");
-		return;
-	}
-	var form = document.getElementById("form1");
-	var uname = form1.uname.value;
-	var msg = form1.msg.value;
-	var userid = form1.userid.value;
-	var xmlhttp = new XMLHttpRequest();
-	
-	xmlhttp.onreadystatechange = function() {
-		if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			document.getElementById('chatlogs').innerHTML = xmlhttp.responseText;
-			form.reset();
-		}
-	}
-	
-	xmlhttp.open('GET','http://localhost/php-chat/insert.php?key=mysend&user_id='+userid+'&uname='+uname+'&msg='+msg,true);
-	xmlhttp.send();
 
-}
 $(document).on('click', '.userchat', function(){  
-       var user_id = $(this).attr("id");
+       var fname = $(this).attr("id");
        var x = document.getElementById("chatlogs");
        var x1 = document.getElementById("chatlogsa");
-       var y = document.getElementById("all");
-       var y = document.getElementById("my");
-       document.getElementById("userid").value = user_id;
-       x.style.display = "none";
-       x1.style.display = "block";
-       y.style.display = "none";
-       y1.style.display = "block";
+       var y = document.getElementById("btn-chata");
+       var y1 = document.getElementById("btn-chatmy");
+       document.getElementById("userid").value = fname;
+	       x.style.display = "none";
+	       x1.style.display = "block";
+	       y.style.display = "none";
+	       y1.style.display = "block";
+       
        $.ajaxSetup({
        	cache: false
        });
-       setInterval( function(){ $('#chatlogsa').load("http://localhost/php-chat/logs.php?key=getmy&user_id="+user_id+"&uname=<?php echo $this->session->userdata('userdata')['fname']; ?>"); }, 2000 ); 
+       setInterval( function(){ $('#chatlogsa').load("http://localhost/php-chat/logs.php?key=getmy&fname="+fname+"&user_id=<?php echo $this->session->userdata('userdata')['id']; ?>"); }, 2000 ); 
   });
 
 </script>
